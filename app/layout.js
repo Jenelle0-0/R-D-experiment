b4ffc65a2e5902c7007f1301531594133f73fb87
Jenelle Davelaar <jd24acm@hert.ac.uk>
2025-10-24 12:39:14 +0200

    fix: correct way of using html tag

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,19 @@ import './globals.css'
 export const metadata = {
   title: 'R-D Experiment',
   description: 'Pre-rendering vs client-side rendering',
+  other: {
+    "google-site-verification": "oEKr8HfUMuU5I5jRnd7RnUfRIzFWHknTTUBVaiYn0jQ",
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="oEKr8HfUMuU5I5jRnd7RnUfRIzFWHknTTUBVaiYn0jQ" />
-      </Head>
-      <body>{children}</body>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
